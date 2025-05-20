59c01ae819cfd12965ab830b43abf6dc033f8671
c0de4un <code4un@yandex.ru>
2025-05-20 22:13:35 +0400

    fix: Add indices for attributes table

--- migrations/m250520_180426_add_attributes_table.php
+++ migrations/m250520_180426_add_attributes_table.php
@@ -16,6 +16,8 @@ class m250520_180426_add_attributes_table extends Migration
             'key'         => $this->string(32),
             'value'       => $this->text(),
         ]);
+
+        $this->createIndex('attributes_owner_idx', 'attributes', 'owner_id, owner_class');
     }
 
     /**
@@ -23,7 +25,10 @@ class m250520_180426_add_attributes_table extends Migration
      */
     public function safeDown()
     {
+        $this->dropIndex('attributes_owner_idx', 'attributes');
+
         $this->dropTable('attributes');
+
         return true;
     }
 }
